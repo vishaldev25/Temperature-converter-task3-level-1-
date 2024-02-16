7d5304dde69041a99fac32a61fefa6502e8efd50
VISHAL <vish2512d@gmail.com>
2024-02-16 18:48:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let inputEl = document.getElementById('input').innerHTML;
+
+const fromEl = document.getElementById('from');
+const toEl = document.getElementById('to');
+const formEl = document.getElementById('form');
+
+
+
+formEl.addEventListener('submit', (event) => { 
+    event.preventDefault();
+})

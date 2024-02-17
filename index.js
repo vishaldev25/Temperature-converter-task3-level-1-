4701a4dac19eba903929ed35e3bab8ba82f46201
VISHAL <vish2512d@gmail.com>
2024-02-17 22:31:21 +0530

    add Footer and result degrees

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let fromEl = document.getElementById('from');
 let toEl = document.getElementById('to');
 let formEl = document.getElementById('form');
 let resultEl = document.getElementById('result');
+let copyrightEl = document.getElementById('copyright');
+const resultDegreeEl = document.getElementById('resultDegree');
+
+const date = new Date().getFullYear()
+copyrightEl.innerHTML = `Copyright © ${date}. All rights reserved.`
 
 let formData = {
     fromValue: fromEl.options[0].value,
@@ -24,18 +29,27 @@ inputEl.addEventListener("change", function (event) {
 
 function calculateDegrees(data) {
 
+    if (data.toValue === toEl.options[0].value) {
+        resultDegreeEl.innerHTML = '&deg;C'
+    }
+    if (data.toValue === toEl.options[1].value) {
+        resultDegreeEl.innerHTML = '&deg;F'
+    }
+    if (data.toValue === toEl.options[2].value) {
+        resultDegreeEl.innerHTML = '&deg;K'
+    }
     if (data.fromValue === fromEl.options[0].value && data.toValue === toEl.options[0].value) {
         let output = (data.inputValue)
-        resultEl.innerHTML = output; 
+        resultEl.innerHTML = output
     }
     if (data.fromValue === fromEl.options[0].value && data.toValue === toEl.options[1].value) {
        let output = ((data.inputValue * 9 / 5) + 32)
-        resultEl.innerHTML = output;
+        resultEl.innerHTML = output
     }
     
     if (data.fromValue === fromEl.options[0].value && data.toValue === toEl.options[2].value) {
         let output = (data.inputValue) + 273.15
-        resultEl.innerHTML = output;
+        resultEl.innerHTML = output
     }
 
     if (data.fromValue === fromEl.options[1].value && data.toValue === toEl.options[0].value) {

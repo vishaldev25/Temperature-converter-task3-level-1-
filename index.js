42ca84e1a67e322e5d2a804e2db82b95c41e3808
VISHAL <vish2512d@gmail.com>
2024-02-17 20:23:48 +0530

    Add Kelvin and Faren functionality

--- index.js
+++ index.js
@@ -5,47 +5,67 @@ let formEl = document.getElementById('form');
 let resultEl = document.getElementById('result');
 
 let formData = {
-    fromValue: "",
-    toValue: "",
+    fromValue: fromEl.options[0].value,
+    toValue: toEl.options[1].value,
     inputValue: 0
 }
 
-//let output = ((inputEl.value * 9 / 5) + 32);
-//resultEl.innerHTML = output
 fromEl.addEventListener("change", function (event) { 
     formData.fromValue = event.target.value
 })
 
 toEl.addEventListener("change", function(event){ 
-    // console.log(event.target.value);
     formData.toValue = event.target.value;
 })
 
 inputEl.addEventListener("change", function (event) {
-    formData.inputValue = event.target.value;
+    formData.inputValue = Number(event.target.value);
 })
 
+function calculateDegrees(data) {
 
-formEl.addEventListener('submit', (event) => { 
-    event.preventDefault();
-    if (formData.fromValue === "") {
-        formData.fromValue = fromEl.options[0].text
+    if (data.fromValue === fromEl.options[0].value && data.toValue === toEl.options[0].value) {
+        let output = (data.inputValue)
+        resultEl.innerHTML = output; 
     }
-    if (formData.toValue === "") { 
-        formData.toValue = toEl.options[1].text
-    }
-    // console.log(formData)
-    if (formData.fromValue === fromEl.options[0].text && formData.toValue === toEl.options[1].text) {
-        // console.log(formData.inputValue)
-        let output = ((formData.inputValue * 9 / 5) + 32)
-        resultEl.innerHTML = output;
-    }
-
-    if (formData.fromValue === fromEl.options[1].text && formData.toValue === toEl.options[0].text) {
-        // console.log(formData.inputValue)
-        let output = ((formData.inputValue - 32) * 5 / 9)
+    if (data.fromValue === fromEl.options[0].value && data.toValue === toEl.options[1].value) {
+       let output = ((data.inputValue * 9 / 5) + 32)
         resultEl.innerHTML = output;
     }
     
+    if (data.fromValue === fromEl.options[0].value && data.toValue === toEl.options[2].value) {
+        let output = (data.inputValue) + 273.15
+        resultEl.innerHTML = output;
+    }
 
+    if (data.fromValue === fromEl.options[1].value && data.toValue === toEl.options[0].value) {
+        let output = ((formData.inputValue - 32) * 5 / 9)
+        resultEl.innerHTML = output;
+    }
+    if (data.fromValue === fromEl.options[1].value && data.toValue === toEl.options[1].value) {
+        let output = (formData.inputValue)
+        resultEl.innerHTML = output;
+    }
+    if (data.fromValue === fromEl.options[1].value && data.toValue === toEl.options[2].value) {
+        let output = ((formData.inputValue - 32) * 5 / 9) + 273.15
+        resultEl.innerHTML = output;
+    }
+
+    if (data.fromValue === fromEl.options[2].value && data.toValue === toEl.options[0].value) {
+        let output = (formData.inputValue - 273.15)
+        resultEl.innerHTML = output;
+    }
+    if (data.fromValue === fromEl.options[2].value && data.toValue === toEl.options[1].value) {
+        let output = ((formData.inputValue - 273.15) * 9 / 5) + 32
+        resultEl.innerHTML = output;
+    }
+    if (data.fromValue === fromEl.options[2].value && data.toValue === toEl.options[2].value) {
+        let output = formData.inputValue
+        resultEl.innerHTML = output;
+    }
+}
+
+formEl.addEventListener('submit', (event) => { 
+    event.preventDefault();
+    calculateDegrees(formData);
 })
